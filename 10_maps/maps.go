3b79642d2ff10bd4988d3087ca204a1334cb3522
SaiDaiwikV <vsaidaiwik@gmail.com>
2025-09-22 12:10:38 +0530

    Maps_Updated

--- 10_maps/maps.go
+++ 10_maps/maps.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"maps"
 	// "slices"
 )
 
@@ -32,4 +33,43 @@ func main(){
 
 	fmt.Println(m1)
 
+	clear(m1)
+
+	fmt.Println(m1)
+
+	m2 := map[string]string{"phone":"Poco F7", "Price":"29k"}
+
+	fmt.Println(m2)
+
+	v1, ok := m2["Price"]
+	v2, kk := m2["mobile"]
+	if ok {
+		fmt.Println("All Okay")
+	}else{
+		fmt.Println("Not Okay")
+	}
+
+	if kk { // kk is not present in m2 map
+		fmt.Println("Phone is Okay")
+	}else{
+		fmt.Println("Phone is not Okay")
+	}
+
+	// printing v1 and v2 values :
+	fmt.Println(v1)
+	fmt.Println(v2)
+
+	// comparing two maps in two different ways:
+
+	n1 := map[string]string{"name":"goalng", "IDE":"VS-CODE"}
+	n2 := map[string]string{"name":"goalng", "IDE":"VS-CODE"}
+	
+
+	fmt.Println(maps.Equal(n1,n2))
+
+	n1 = map[string]string{"name":"goalng", "IDE":"VS-CODE"}
+	n2 = map[string]string{"name":"CPP", "IDE":"VS-CODE"}
+
+	fmt.Println(maps.Equal(n1,n2))
+	
 }
