ab092e1bc6dc6361f6e67b94d6b7c5f0b2030c85
SaiDaiwikV <vsaidaiwik@gmail.com>
2025-09-23 11:13:23 +0530

    functions

--- 12_functions/functions.go
+++ 12_functions/functions.go
@@ -8,8 +8,20 @@ func add(a int,b int)int{
 	return a + b
 }
 
+func getlanguages() (string, string, string){
+	return "golang", "python", "cpp"
+}
+
 func main(){
 	res := add(2, 3)
-	
+
 	fmt.Println(res)
+
+	lang1 , lang2 , lang3 := getlanguages()
+
+	fmt.Println(lang1, lang2, lang3)
+
+	// or you can simply call this function in fmt.Println directly
+	
+	fmt.Println(getlanguages())
 }
