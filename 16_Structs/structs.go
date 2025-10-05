0358b91dd7fa1925d425a83aa61e85a38e994aa1
SaiDaiwikV <vsaidaiwik@gmail.com>
2025-10-06 01:15:39 +0530

    Updated structs

--- 16_Structs/structs.go
+++ 16_Structs/structs.go
@@ -16,6 +16,16 @@ type order struct {
 // i need to create a link between function and struct
 // receiver type:
 
+func newOrder(id string, amount float32, status string) *order {
+	Myorder := order{
+		id:     id,
+		amount: amount,
+		status: status,
+	}
+
+	return &Myorder
+}
+
 func (o *order) changeStatus(status string) { // the syntax is we need to use first letter of struct for example i used 'o' because my struct name is 'order'
 	o.status = status
 }
@@ -65,4 +75,11 @@ func main() {
 	fmt.Println("The amount of order-1 is : ", myorder1.getAmount())
 
 	fmt.Println("The amount of order-2 is :  ", myorder2.getAmount())
+
+	fmt.Println("-----------------------------")
+
+	// creating myorder3 using constructor of struct:
+	myorder3 := newOrder("3", 666.99, "Order Initiated")
+
+	fmt.Println("Order-3 is : ", myorder3)
 }
